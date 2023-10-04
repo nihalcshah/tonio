c2d78f3a712352b69632e68fe6ecac2ff90668a3
nihalcshah <nihalcshah@gmail.com>
2023-10-04 03:38:10 -0400

    hidden gallery 3

--- src/Inf.js
+++ src/Inf.js
@@ -489,11 +489,11 @@ function Info() {
             <div className="md:p-5 p-1 m-auto w-5/6 ">
                 <div className="font-bold md:text-4xl text-2xl">Customize</div>
                 <div className="font-medium md:text-md text-sm">Create a Tone Swatch</div>
-                <button onClick={OpenGallery} className="flex p-1.5 rounded-lg bg-black text-white hover:bg-white hover:text-black gap-2 mt-2">
+                <button onClick={OpenGallery} data-modal-target="galleryModal" data-modal-toggle="galleryModal" className="flex p-1.5 rounded-lg bg-black text-white hover:bg-white hover:text-black gap-2 mt-2">
                     <svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3 m-auto" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                     </svg>
-                    <div className="text-sm" data-modal-target="galleryModal" data-modal-toggle="galleryModal">Visit Gallery For Colors</div>
+                    <div className="text-sm" >Visit Gallery For Colors</div>
                 </button>
                 <div className="grid md:grid-cols-2">
                     <div className="md:p-8 p-4 flex flex-col items-center"  >
